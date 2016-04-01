b435ca042588b98c24fefb5a1e13bd055ffb631a
yulizar <Erwin@10.5.249.53>
2016-04-01 17:08:27 +0700

    Erwin Yulizar F || 1301144031

---
+++ MusicPlayer/src/driver/Driver.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
+
+import control.Controller;
+
+
+/**
+ *
+ * @author Erwin
+ */
+public class Driver {
+    public static void main(String[] args) {
+        Controller control = new Controller();
+        
+    }
+}
