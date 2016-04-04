5a4b43542266e1ce4e4f4dec729aba89629cc920
Thoriq <>
2016-04-04 19:40:13 +0700

    Task selesai, class sudah dibuat dan di testing music Player berjalan normal

---
+++ MusicPlayer/src/musicplayer/Driver.java
@@ -0,0 +1,13 @@
+package musicplayer;
+
+/**
+ *
+ * @author Thoriq
+ */
+public class Driver {
+    
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+    }
+    
+}
