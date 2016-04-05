d611c6fad473581d110a1f4ded2f8d0840583370
irfantrianto <irfanttrianto@gmail.com>
2016-04-05 22:36:37 +0800

    Task 5 Done

---
+++ MusicPlayer/src/musicplayer/Application.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musicplayer;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+/**
+ *
+ * @author Irfan
+ */
+public class Application {
+    private ArrayList<String> musicList;
+    private ThreadPlayer thread;
+    private int nowPlaying;
+
+    class ThreadPlayer extends Thread {
+
+        String filepath;
+        Player player;
+
+        public ThreadPlayer(String filepath) {
+            this.filepath = filepath;
+        }
+
+        public void run() {
+            try {
+                FileInputStream fis = new FileInputStream(filepath);
+                player = new Player(fis);
+                player.play();
+            } catch (FileNotFoundException e) {
+                throw new IllegalStateException("File " + filepath + " not found");
+
+            } catch (JavaLayerException e) {
+                throw new IllegalStateException("ERROR while playing file");
+            }
+        }
+    }
+    
+    public Application() {
+        musicList = new ArrayList<>();
+    }
+    
+    public void addMusic(String path){
+        musicList.add(path);
+    }
+    
+    public String[] getMusicList(){
+        return (String[]) musicList.toArray(new String[0]);
+    }
+    
+    public String getNowPlayed(){
+        return musicList.get(nowPlaying);
+    }
+    
+    public void removeMusic(int i){
+        musicList.remove(i);
+    }
+    
+    public void play(int i)
+    {
+        nowPlaying = i;
+        String filepath = musicList.get(i);
+        thread = new ThreadPlayer(filepath);
+        thread.start();
+    }
+    
+    public void stop(){
+        if(thread != null){
+            thread.stop();
+        }
+    }
+    
+    public void next(){
+        if(nowPlaying< musicList.size()-1){
+            stop();
+            play(nowPlaying+1);
+        }
+    }
+    public void prev(){
+        if(nowPlaying< musicList.size()+1){
+            stop();
+            play(nowPlaying-1);
+        }
+    }
+
+}
