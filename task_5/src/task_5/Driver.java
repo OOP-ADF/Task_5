b4b273ade56a0a2cb08ce320f10ce16e2c2435d1
asus <asus@asus-PC.connectify>
2016-04-13 20:23:31 +0700

    Merge origin/master
    Conflicts:
    	TubesPBO/src/tubespbo/Dokter.java

---
+++ task_5/src/task_5/Driver.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task_5;
+
+/**
+ *
+ * @author asus
+ */
+public class Driver {
+     public static void main(String[] args) {
+        Controller controller = new Controller();
+}
+}
