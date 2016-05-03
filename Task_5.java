d5439e399485d514e9546a7c04709b78bf9852a6
Gctatmaja <genturcta@gmail.com>
2016-05-03 14:49:05 +0700

    Create Task_5.java

---
+++ Task_5.java
@@ -0,0 +1,19 @@
+
+ package task_5;
+ 
+ import Controller.Controller;
+ 
+ /**
+  *
+  * @author Gentur Cipto Tri Atmaja
+  * IF 38 09 / 1301140239
+  */
+  
+ public class Task_5 
+ {
+    public static void main(String[] args)
+	  {
+        Controller controller = new Controller();
+    }
+ }
+	
