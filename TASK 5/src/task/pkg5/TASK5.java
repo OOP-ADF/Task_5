59820c12bf302a4aaf3f4beb7d318d471a5ae732
abdulnursahid <abdul.nursahid@gmail.com>
2017-05-17 22:36:24 +0700

    Abdul Nur Sahid _1301154385_IF39-07
    Remedial CLO3

---
+++ TASK 5/src/task/pkg5/TASK5.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task.pkg5;
+
+/**
+ *
+ * @author abdulnursahid
+ */
+public class TASK5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
